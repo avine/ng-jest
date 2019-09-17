d7edcb6fe2148afa82363d8edb644739a23d6b3f
Stéphane Francel <contact@avine.io>
2019-09-17 17:24:31 +0200

    Add HomeComponent

---
+++ src/app/components/home/home.component.spec.ts
@@ -0,0 +1,30 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { HomeComponent } from './home.component';
+
+describe('HomeComponent', () => {
+  let component: HomeComponent;
+  let fixture: ComponentFixture<HomeComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ HomeComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(HomeComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should display H1', () => {
+    const computed: HTMLElement = fixture.debugElement.nativeElement;
+    expect(computed.querySelector('h1').textContent).toBe('Welcome to Angular with Jest');
+  });
+});
